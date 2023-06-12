43baf1507dbe674ba24bd70722c5542924c71f1f
Svein Gunnar Larsen <svein.gunnar.larsen@neptune-software.com>
2023-06-12 13:41:08 +0200

    test new image path and backward compatability

---
+++ artifacts/Application/fileuploaddemo/script/neptune.Script/Javascript.js
@@ -0,0 +1,17 @@
+async function importDeploymenFile(evt) {
+    if (evt.target.files.length < 1) {
+        return;
+    }
+
+    console.log(evt.target.files[0]);
+
+    const formData = new FormData();
+    formData.append('myFile', evt.target.files[0]);
+
+    await fetch('/api/serverscript/postimage/save', {
+        method: 'POST',
+        body: formData,
+    });
+}
+
+window.importDeploymenFile = importDeploymenFile;
